f07f44e61bc14029658a19eef091894da0f3f4d9
OlhaOvsiienko <137906412+OlhaOvsiienko@users.noreply.github.com>
2023-09-08 13:01:34 +0000

    correct Task 6, js

--- 06-DOM-Tasks/06-DOM-TASK-6/task-06.js
+++ 06-DOM-Tasks/06-DOM-TASK-6/task-06.js
@@ -1,41 +1,52 @@
 const colorElements = document.querySelectorAll('.color');
+const sizeDropdown = document.getElementById('sizeDropdown');
+const typeDropdown = document.getElementById('typeDropdown');
 const priceElement = document.getElementById('outprice');
 
-let currentPrice = parseFloat(priceElement.textContent);
+let currentPrice = 170;
+let currentColor = 'blue';
 
 function updatePrice() {
-  priceElement.textContent = currentPrice.toFixed(2); 
+    const selectedSize = sizeDropdown.value;
+    const selectedType = typeDropdown.value;
+         
+    let sizeCoefficient = 1;
+    if (selectedSize === 'medium') {
+        sizeCoefficient = 1.1;
+    } else if (selectedSize === 'large') {
+        sizeCoefficient = 1.2;
+    }
+
+    let typeCoefficient = 1;
+    if (selectedType === 'outdoor') {
+        typeCoefficient = 1.25;
+    }
+    
+    const colorElement = document.querySelector(`.color[color="${currentColor}"]`);
+    const newPrice = parseFloat(colorElement.getAttribute('data-price'));
+   
+    const updatedPrice = newPrice * sizeCoefficient * typeCoefficient;
+    priceElement.textContent = updatedPrice.toFixed(2);
 }
 
 colorElements.forEach(function(colorElement) {
-  colorElement.addEventListener('click', function() {
-    const newPrice = parseFloat(colorElement.getAttribute('data-price'));
-    if (!isNaN(newPrice)) {
-      currentPrice = newPrice;
-      updatePrice();
-    }
-    
-    colorElements.forEach(function(element) {
-      element.classList.remove('active');
+    colorElement.addEventListener('click', function() {
+        
+        currentColor = colorElement.getAttribute('color');
+        
+        updatePrice();
+
+        colorElements.forEach(function(element) {
+            element.classList.remove('active');
+        });
+        colorElement.classList.add('active');
     });
-    colorElement.classList.add('active');
-  });
 });
 
-// Variation 1: size
-const sizeDropdown = document.getElementById('sizeDropdown');
-sizeDropdown.addEventListener('change', function() {
-  const selectedSize = sizeDropdown.value;
-  
-  currentPrice += 10;
-  updatePrice();
-});
+sizeDropdown.addEventListener('change', updatePrice);
+typeDropdown.addEventListener('change', updatePrice);
 
-// Variation 2: type
-const typeDropdown = document.getElementById('typeDropdown');
-typeDropdown.addEventListener('change', function() {
-  const selectedType = typeDropdown.value;
-  
-  currentPrice += 20;
-  updatePrice();
-});
+const initialSize = sizeDropdown.value;
+const initialType = typeDropdown.value;
+
+updatePrice();
