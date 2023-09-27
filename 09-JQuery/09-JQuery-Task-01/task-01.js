dec52aee058ad06eb4005fd41568ad1b13009990
OlhaOvsiienko <137906412+OlhaOvsiienko@users.noreply.github.com>
2023-09-27 22:56:52 +0300

    Update task-01.js

--- 09-JQuery/09-JQuery-Task-01/task-01.js
+++ 09-JQuery/09-JQuery-Task-01/task-01.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
-    $("h2.head").css("background-color", "green");
-    $("h2.head .inner").css("font-size", "35px");
+    let headerElements = $("h2.head");
+    headerElements.css("background-color", "green");
+    headerElements.children(".inner").css("font-size", "35px");
 });
